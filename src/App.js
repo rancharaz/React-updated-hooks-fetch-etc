f706e4af1af10b68b251599f2968937e8b7884c4
rancharaz <ranveer1990@hotmail.com>
2022-07-18 00:25:05 +0400

    working with render props

--- src/App.js
+++ src/App.js
@@ -51,8 +51,31 @@ const Lift = ({ name, elevationGain, status }) => {
   )
 }
  */
-function App() {
+/* 
+  render props
 
+const tahoe_peaks = [
+  { name: "Freel", elevation: 10891 },
+  { name: "Monument", elevation: 10067 },
+  { name: "Pyramid", elevation: 9983 },
+  { name: "Tallac", elevation: 9735 },
+
+]
+
+function List({ data, renderItem, renderEmpty }) {
+  return !data.length ? renderEmpty : <ul>
+    {data.map((item) => (
+      <li key={item.name}>
+        {renderItem(item)}
+      </li>
+    )
+
+    )}
+  </ul>
+}
+ */
+
+function App() {
 
   /*   
     graphQL
@@ -222,6 +245,16 @@ function App() {
           <button>Add</button>
         </form>
  */}
+
+
+        {/* 
+        render props
+        
+        <List data={tahoe_peaks} renderEmpty={<p>This list is empty</p>}
+          renderItem={item => <> {item.name} - {item.elevation} ft</>}
+
+        /> */}
+
 
 
       </header>
